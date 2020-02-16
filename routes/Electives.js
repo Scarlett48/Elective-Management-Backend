201359f75631d85b97669ec9c3779c81fad5fb22
unknown <msaipriyadharshini@gmail.com>
2020-02-17 00:58:04 +0530

    commit

--- routes/Electives.js
+++ routes/Electives.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const mysqlConnection = require("../database");
 
 router.post("/addElectives",(req,res)=>{
-        var dept_name = req.body.dept_name;
+        var department = req.body.department;
         var sem = req.body.sem;
         var course_code = req.body.course_code;
         var course = req.body.course;
     
-        mysqlConnection.query("INSERT INTO electives VALUES (\""+dept_name+"\","+sem+",\""+course_code+"\",\""+course+"\")", (err,result)=>{
+        mysqlConnection.query("INSERT INTO electives VALUES (\""+department+"\","+sem+",\""+course_code+"\",\""+course+"\")", (err,result)=>{
             if(!err){
                 res.send(true);
                 console.log("Inserted Data Successfully");
@@ -20,5 +20,33 @@ router.post("/addElectives",(req,res)=>{
             }
         });
     });
+
+router.post("/deleteElectives",(req,res)=>{
+    var course_code = req.body.course_code;
+
+    mysqlConnection.query("SELECT * FROM electives WHERE course_code = \""+course_code+"\"",(err,result)=>{
+        if(!err){
+            if(result.length>0){
+                mysqlConnection.query("DELETE FROM electives WHERE course_code = \""+course_code+"\"",(err,result)=>{
+                    if(!err){
+                        res.send(true);
+                    }
+                    else{
+                        res.send(false);
+                        console.log(err);
+                    }
+                });
+            }
+            else{
+                res.send("NO SUCH COURSE EXISTS");
+            }
+        }
+        else{
+            res.send(false);
+            console.log(err);
+        }
+    });
+
+});
 
 module.exports= router;
