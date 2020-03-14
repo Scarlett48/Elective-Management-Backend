cad2859bd431317af53e95ce31a8325b4b4b9360
unknown <msaipriyadharshini@gmail.com>
2020-03-15 00:46:18 +0530

    unit testing for all USER routes done

--- test/users.test.js
+++ test/users.test.js
@@ -6,8 +6,9 @@ const app = require('../server');
 describe('POST /Login', ()=>{
     it('Check if all details are returned when correct login credentials are provided', (done)=>{
         request(app).post('/Login')
-        .send({rollno:'CB.EN.U4CSE17250', pass:'hi'})
+        .send({rollno:'CSE17250', pass:'hi'})
         .then((res)=>{
+            // assert.equal(response.status, 200);
             expect(res.body).to.contain.property("name");
             expect(res.body).to.contain.property("rollno");
             expect(res.body).to.contain.property("password");
@@ -18,9 +19,9 @@ describe('POST /Login', ()=>{
         .catch((err) => done(err));
     });
 
-    it('Check if WRONG PASSWORD returned when incorrect password is provided', (done)=>{
+    it('WRONG PASSWORD returned when incorrect password is provided', (done)=>{
         request(app).post('/Login')
-        .send({rollno:'CB.EN.U4CSE17250', pass:'hfi'})
+        .send({rollno:'CSE17250', pass:'hfi'})
         .then((res)=>{
             expect(res.text).to.contain("WRONG PASSWORD");
             done();
@@ -28,9 +29,9 @@ describe('POST /Login', ()=>{
         .catch((err) => done(err));
     });
 
-    it('Check if NO SUCH ROLL NUMBER EXISTS returned when incorrect username is provided', (done)=>{
+    it('NO SUCH ROLL NUMBER EXISTS returned when incorrect username is provided', (done)=>{
         request(app).post('/Login')
-        .send({rollno:'CB.EN.U4CSE17950', pass:'hello'})
+        .send({rollno:'CSE17950', pass:'hello'})
         .then((res)=>{
             expect(res.text).to.contain("NO SUCH ROLL NUMBER EXISTS");
             done();
@@ -43,7 +44,7 @@ describe('POST /Login', ()=>{
 describe('POST /Register', ()=>{
     it('registration of duplicate entry should not be allowed', (done)=>{
         request(app).post('/Register')
-        .send({name:'sai', rollno:'CB.EN.U4CSE17250', pass:'hi', sem:6, sec:'b'})
+        .send({name:'sai', rollno:'CSE17250', pass:'hi', sem:6, sec:'b'})
         .then((res)=>{
             expect(res.text).to.contain("USER ALREADY EXISTS");
             done();
@@ -51,3 +52,28 @@ describe('POST /Register', ()=>{
         .catch((err) => done(err));
     });
 });
+
+describe('POST /deleteUser', ()=>{
+    it("if a roll number that doesn't exist is given as input for deleting, NO SUCH ROLL NUMBER EXISTS should be returned", (done)=>{
+        request(app).post('/deleteUser')
+        .send({rollno:'cse123456'})
+        .then((res) =>{
+            expect(res.text).to.contain('NO SUCH ROLL NUMBER EXISTS');
+            done();
+        })
+        .catch((err)=> done(err));
+    });
+});
+
+describe('POST /editPassword', ()=>{
+    it("if old password is wrong, return WRONG PASSWORD", (done)=>{
+        request(app).post('/editPassword')
+        .send({rollno:'cse17211', oldPass:'rip123', newPass:'QWERTY1234'})
+        .then((res)=>{
+            console.log(res.text);
+            expect(res.text).to.contain('WRONG PASSWORD');
+            done();
+        })
+        .catch((err)=> done(err));
+    });
+})
