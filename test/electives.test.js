d34c22f57075ca69aa031c19f2031fb93757b7af
unknown <msaipriyadharshini@gmail.com>
2020-03-15 03:06:05 +0530

    unit testing of ELECTIVES routes

---
+++ test/electives.test.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+const expect = require('chai').expect;
+const request = require('supertest');
+const app = require('../server');
+
+describe('POST /deleteElectives', ()=>{
+    it("return NO SUCH COURSE EXISTS when a course that doesn't exist in DB is deleted", (done)=>{
+        request(app).post('/deleteElectives')
+        .send({course_code:'12ece45671'})
+        .then((res)=>{
+            expect(res.text).to.contain("NO SUCH COURSE EXISTS");
+            done();
+        })
+        .catch(done);
+    });
+})
