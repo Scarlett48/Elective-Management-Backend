15010ad7978f68a2c5a684c6d06928962babd3f6
unknown <msaipriyadharshini@gmail.com>
2020-03-15 00:48:04 +0530

    change Elective preference route added

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const login = require('./routes/Users');
 const deleteUser = require('./routes/Users');
 const register = require('./routes/Users');
 const chooseElectivePreference = require('./routes/PreferenceList');
+const changeElectivePreference = require('./routes/PreferenceList');
 const editPass = require('./routes/Users');
 
 app.use(bodyParser.json());
@@ -23,11 +24,12 @@ app.post("/Register",register);
 app.post("/Login",login);
 app.post("/deleteUser",deleteUser);
 app.post("/viewElectives",viewElectives);
+app.post("/student/viewElectives",studentviewElectives);
 app.post("/addElectives",addElective);
 app.post("/addOneElective", addOneElective);
 app.post("/deleteElectives",deleteElective);
-app.post("/student/viewElectives",studentviewElectives);
 app.post("/chooseElectivePreference",chooseElectivePreference);
+app.post("/changeElectivePreference",changeElectivePreference)
 app.post("/editPassword",editPass);
 
 app.listen(3001);
