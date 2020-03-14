19e255f224e41d3cb5e4d80d39af01995c3baf34
unknown <msaipriyadharshini@gmail.com>
2020-03-15 00:48:46 +0530

    new route added

--- routes/PreferenceList.js
+++ routes/PreferenceList.js
@@ -17,7 +17,37 @@ router.post("/chooseElectivePreference",(req,res)=>{
         else{
             res.send(false);
             console.log("Failed to submit preferences");
-            console.log(err);
+            // console.log(err);
+        }
+    });
+
+});
+
+router.post("/changeElectivePreference",(req,res)=>{
+    var rollno = req.body.rollno;
+    var elec1 = req.body.elec1;
+    var elec2 = req.body.elec2;
+    var elec3 = req.body.elec3;
+
+    mysqlConnection.query("DELETE FROM preferencelist WHERE rollno = \'"+rollno+"\'" , (err,result)=>{
+        if(!err){
+            console.log("old preference deleted");
+        }
+        else{
+            console.log("error deleting old preference");
+            // console.log(err);
+        }
+    });
+    
+    mysqlConnection.query("INSERT INTO preferencelist(rollno,course_code_pref1,course_code_pref2,course_code_pref3) VALUES (\""+rollno+"\",\""+elec1+"\",\""+elec2+"\",\""+elec3+"\")", (err,result)=>{
+        if(!err){
+            res.send(true);
+            console.log("Preference submitted Successfully");
+        }
+        else{
+            res.send(false);
+            console.log("Failed to submit preferences");
+            // console.log(err);
         }
     });
 
